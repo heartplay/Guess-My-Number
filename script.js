79c28d44ac4e477983791fb9ed073d206fc38275
heartplay <168409591+heartplay@users.noreply.github.com>
2025-02-03 11:47:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent)
+// document.querySelector('.message').textContent = `🥳 Correct Number!`
+// console.log(document.querySelector('.message').textContent)
+
+// document.querySelector('.number').textContent = 13
+// document.querySelector('.score').textContent = 10
+
+// document.querySelector('.guess').value = 23
+// console.log((document.querySelector('.guess').value = 23))
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(`.number`).textContent = secretNumber;
+
+document.querySelector(`.check`).addEventListener(`click`, function () {
+    const guess = Number(document.querySelector(`.guess`).value);
+    if (!guess) {
+        document.querySelector(`.message`).textContent = `No number! 🚫`;
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = `🥳 Correct Number!`;
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = `📈 Too high!`;
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector(
+                '.message'
+            ).textContent = `💥 You lost the game!`;
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = `📈 Too low!`;
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector(
+                '.message'
+            ).textContent = `💥 You lost the game!`;
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
