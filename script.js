aabb1888554ac50bcf85774622fa47a7176aac9d
Gleb Fedoseev <heartplay.gf@gmail.com>
2025-02-03 12:02:50 +0500

    Code comments
    Removing

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent)
-// document.querySelector('.message').textContent = `🥳 Correct Number!`
-// console.log(document.querySelector('.message').textContent)
-
-// document.querySelector('.number').textContent = 13
-// document.querySelector('.score').textContent = 10
-
-// document.querySelector('.guess').value = 23
-// console.log((document.querySelector('.guess').value = 23))
-
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
